06c8aec2040bdeb9d0b029461ad1befec725dd8a
Philipp Winter <phw@brave.com>
2022-05-23 07:55:22 -0700

    Add -debug switch that prints other PCR values.

--- main.go
+++ main.go
@@ -50,11 +50,13 @@ func fetchAttestationDocument(nonce, attestationEndpoint string) ([]byte, error)
 
 func main() {
 	var attestationFile, attestationEndpoint string
+	var debug bool
 	var err error
 	var rawDoc []byte
 
 	flag.StringVar(&attestationFile, "file", "", "File that contains a Base64-encoded attestation document.")
 	flag.StringVar(&attestationEndpoint, "url", "", "Attestation endpoint of the Nitro enclave..")
+	flag.BoolVar(&debug, "debug", false, "Print extra debug information.")
 	flag.Parse()
 
 	nonce := make([]byte, nonceSize)
@@ -88,10 +90,18 @@ func main() {
 	}
 	log.Println("Correct nonce found in attestation document.")
 
-	pcr0, exists := doc.Document.PCRs[0]
-	if !exists {
-		log.Fatal("Attestation document does not contain PCR0.")
+	if debug {
+		// See the following page for a description of the following values:
+		// https://docs.aws.amazon.com/enclaves/latest/user/set-up-attestation.html
+		for _, i := range []uint{0, 1, 2, 3, 4, 8} {
+			fmt.Printf("PCR[%d]: %x\n", i, doc.Document.PCRs[i])
+		}
+	} else {
+		pcr0, exists := doc.Document.PCRs[0]
+		if !exists {
+			log.Fatal("Attestation document does not contain PCR0.")
+		}
+		log.Println("Remote PCR0:")
+		fmt.Printf("%x\n", pcr0)
 	}
-	log.Println("Remote PCR0:")
-	fmt.Printf("%x\n", pcr0)
 }
